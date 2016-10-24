a11181c13447906652cdab5960a17f8840d09f13
chenjianhui <chenjianhui@meituan.com>
2016-10-24 19:42:42 +0800

    less-loader-test

--- src/js/page/index.js
+++ src/js/page/index.js
@@ -1,7 +1,18 @@
 /**
  * Created by chenjianhui on 16/10/10.
  */
+import React from 'react';
+import classnames from 'classnames';
+import ReactDom from 'react-dom';
+import styles from './style.less';
 
-import './style.css';
-
-alert("hello");
+console.log(styles);
+ReactDom.render(
+    <div>
+        <div className={"list"}>
+            hello world
+        </div>
+    </div>
+    ,
+    document.getElementById("main-container")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
 module.exports = {
     //插件项
-    plugins: [commonsPlugin],
+    plugins: [commonsPlugin,[{ "libraryName": "antd", "style": true }]],
     //页面入口文件配置
     entry: {
         index : './src/js/page/index.js'
@@ -17,19 +17,17 @@ module.exports = {
         //加载器配置
         loaders: [
             { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.js$/, loader: 'babel',  exclude: /(node_modules|bower_components)/, query: { presets: ['es2015'] }},
+            { test: /\.js$/, loader: 'babel',  exclude: /(node_modules|bower_components)/, query: { presets: ['react','es2015'] }},
             { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            { test: /\.less$/, loader: 'style!css!less?sourceMap'},
             { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
         ]
     },
     //其它解决方案配置
     resolve: {
-        root: 'E:/github/flux-example/src', //绝对路径
         extensions: ['', '.js', '.json', '.scss'],
         alias: {
-            AppStore : 'js/stores/AppStores.js',
-            ActionType : 'js/actions/ActionType.js',
-            AppAction : 'js/actions/AppAction.js'
+            
         }
     }
 };
